8a44f864776557727ea8377c41b17b3c32f54f22
your_AbdulKarim <kar92722@gmail.com>
2022-12-30 20:12:01 +0500

    react_porduct_cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Navbar from './componets/NAVBAR/Navbar';
+ import Navbar from './componets/NAVBAR/Navbar';
 import './app.css';
-import Section1 from './componets/Main/Section1';
-import Section2 from './componets/Main/Section2';
-import Contact from './componets/Main/Contact';
-import Footer from './componets/Main/Footer';
+import Cards from './componets/Main/Cards';
+import Image from './componets/Main/Image/Img8.png' 
+import Img7 from './componets/Main/Image/Img7.jpg' 
+import Img3 from './componets/Main/Image/Img3.jpg'
+import Img2 from './componets/Main/Image/Img2.png'
+import Img4 from './componets/Main/Image/Img4.jpg'
+import Img5 from './componets/Main/Image/Img5.jpg'
+import Img6 from './componets/Main/Image/Img6.jpg'
+import Img1 from './componets/Main/Image/Img1.png'
+
+var sdata =[{'Image':Image},{'Image':Img3},{'Image':Img7},{'Image':Img2},{'Image':Img4},{'Image':Img5},{'Image':Img6},{'Image':Img1}]
 
 
 
@@ -13,10 +20,17 @@ function App() {
   return(
     <>
     <Navbar/>
-    <Section1/>
-    <Section2/>
-    <Contact/>
-    <Footer/>
+   
+   {sdata.map((item) => {
+    return <Cards img={item.Image}/>
+
+   })}
+       
+  
+   
+  
+  
+
   </>
   )
 }
